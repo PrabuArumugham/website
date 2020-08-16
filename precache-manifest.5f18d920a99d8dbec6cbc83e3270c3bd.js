138a06a6e884d80722d45d25843204223eb18ee4
PrabuArumugham <69190275+PrabuArumugham@users.noreply.github.com>
2020-08-16 19:55:53 +0530

    Updates

---
+++ precache-manifest.5f18d920a99d8dbec6cbc83e3270c3bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a577c0fc5da242c3699d95736b9f821",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "8270b404d1d80ec3da57",
+    "url": "/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/website/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8270b404d1d80ec3da57",
+    "url": "/website/static/js/main.7bedc16e.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
